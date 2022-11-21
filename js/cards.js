43f20209b27e0ee9e460e9857b61840f307c70e0
Cata Allende <franallende2000@gmail.com>
2022-11-21 12:47:28 -0300

    cartas con boot terminadas

--- js/cards.js
+++ js/cards.js
@@ -4,35 +4,44 @@ window.addEventListener("load", buildCards);
 function buildCards() {
     const $cardContainer = document.getElementById("container_cards");
     let data = JSON.parse(localStorage.getItem("lista_anuncios"));
-    let contador = 0;
+    let contador = 1;
 
-    //va queriendo. No se que tan necesario es el div row
     let $divRow = document.createElement("div");
     $divRow.classList.add("row");
-    //let $divCuatro = document.createElement("div");
-    //$divCuatro.classList.add("col-4");
+    $divRow.classList.add("p-2");
     $cardContainer.appendChild($divRow);
-    //$divRow.appendChild($divCuatro);
 
     if(data != null){
         data.forEach(element => {
-            let $card = document.createElement("div");
-            $card.classList.add("card");
-            $card.innerHTML = `
-                <h3> ${element.titulo} </h3>
-                <h4> ${element.descripcion} </h4>
-                <ul class="ul-card">
-                    <li>Precio: $${element.precio}</li><br>
-                    <li><img src="./imagenes/icono_puerta.png" alt="icono puerta" class="icono"> ${element.puertas} puertas </li>
-                    <li><img src="./imagenes/icono_kms.png" alt="icono kms" class="icono"> ${element.kms} KMs</li>
-                    <li><img src="./imagenes/icono_potencia.png" alt="icono potencia" class="icono"> ${element.potencia} de potencia</li>
-                    </ul>
-                    <button class="btn_ver_vehiculo">Ver Vehiculo</button>`
+            $divRow.innerHTML += `
+              <div class="col-3">
+                  <div class="card text-center border-dark">
+                    <div class="card-title">
+                      <h3> ${element.titulo} </h3>
+                      <h4 class="card-subtitle mb-2"> ${element.descripcion} </h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Precio: $${element.precio}</li>
+                        <li class="list-group-item"><img src="./imagenes/icono_puerta.png" alt="icono puerta" class="icono"> ${element.puertas} puertas </li>
+                        <li class="list-group-item"><img src="./imagenes/icono_kms.png" alt="icono kms" class="icono"> ${element.kms} KMs</li>
+                        <li class="list-group-item"><img src="./imagenes/icono_potencia.png" alt="icono potencia" class="icono"> ${element.potencia} de potencia</li>
+                      </ul>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-primary">Ver Vehiculo</button>
+                    </div>
+                    
+                  </div>
+              </div
+            `
 
-            $divRow.appendChild($card);
-
-            $divRow.appendChild($card);
-            //$cardContainer.appendChild($card);
+            if((contador % 4) == 0){
+                $cardContainer.innerHTML+=`</div>`; //cierro la row que itera hasta 4 cartas
+                $divRow.innerHTML = ``; //lo vacio o se acumulan
+                $cardContainer.appendChild($divRow); //le añado una nueva row 
+            }
+            
             contador++;
         });
     }
